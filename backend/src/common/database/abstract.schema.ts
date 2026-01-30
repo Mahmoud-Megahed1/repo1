30a54d75c3822d29486a78bcdd80094ea807a169
Mahmoud Megahed <your_email@example.com>
2026-01-30 23:15:22 +0200

    fix: remove required constraint from AbstractDocument _id

--- backend/src/common/database/abstract.schema.ts
+++ backend/src/common/database/abstract.schema.ts
@@ -11,7 +11,7 @@ import { SchemaTypes, Types } from 'mongoose';
   },
 })
 export class AbstractDocument {
-  @Prop({ type: SchemaTypes.ObjectId, required: true })
+  @Prop({ type: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
 
   createdAt?: Date;
