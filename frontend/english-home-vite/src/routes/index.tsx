62ee7f216468e8297ced9bfd386e36765f7cd511
Mahmoud Megahed <your_email@example.com>
2026-06-16 13:25:23 +0300

    fix(routing): redirect root to /ar by default

--- frontend/english-home-vite/src/routes/index.tsx
+++ frontend/english-home-vite/src/routes/index.tsx
@@ -1,6 +1,9 @@
-import { LandingPage } from '@modules/landing/components/landing-page';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  component: LandingPage,
+  beforeLoad: () => {
+    throw redirect({
+      to: '/ar',
+    });
+  },
 });
